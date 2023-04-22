286a34db96dbe948b31fd289cb434fff1800742c
kolexstrings <66323820+kolexstrings@users.noreply.github.com>
2023-04-22 06:04:43 +0100

    hardhat config script setup done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("@dotenv").config();
+
+const SEPOILA_URL = process.env.SEPOILA;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepoila: {
+      url: "Sepoila URL goes here",
+      account: [private_key]
+    }
+  }
 };
